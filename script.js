5e4a5a05d56fd8f885e131e2ccccb84a89757157
HenriqueeLimaa <riquebarcelloslima@gmail.com>
2022-02-03 19:59:48 -0300

    set the basic hover effect

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ function getSquareSize(){
 //reminder: if you change the dimensions(example, from 16x16 to 64x64) you have to 
 //change the number of loops. You get the amount of loops making the number of divs * 2
 
+
 for(let i=1;i<=256;i++){
     const div = document.createElement('div');
+    div.classList.add('drawingDivs');
     div.style.flex = "1 0 auto";
     div.style.flexWrap = "wrap";
     div.style.minWidth = getSquareSize();
     div.style.minHeight = getSquareSize();
     container.appendChild(div);
 }
+
+// set the hover effect
+container.addEventListener('mouseover', () => {
+    document.querySelector('.drawingDivs:hover').style.backgroundColor = "black";
+});
