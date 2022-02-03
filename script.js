c4794de27c6b534add8bf1f1be55c3212f2ce106
HenriqueeLimaa <riquebarcelloslima@gmail.com>
2022-02-03 18:41:24 -0300

    Fix it so now its ready to receive some type of input defining the amount of divs will fit in the container

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const container = document.querySelector('div');
 
-const maxDivHeightWidth = () => {
-    return 500 / 16;
+function getSquareSize(){
+    return 500 / 16 + "px";
 }
-
+//reminder: if you change the dimensions(example, from 16x16 to 64x64) you have to 
+//change the number of loops. You get the amount of loops making the number of divs * 2
 
 for(let i=1;i<=256;i++){
     const div = document.createElement('div');
     div.style.flex = "1 0 auto";
     div.style.flexWrap = "wrap";
-    div.style.minWidth = "31.25px";
-    div.style.minHeight = "31.25px";
+    div.style.minWidth = getSquareSize();
+    div.style.minHeight = getSquareSize();
     container.appendChild(div);
 }
